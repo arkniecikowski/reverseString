94cbf6838e507286c3fde7d9d0ab74ce30093871
Arej <arej@MacBook-Pro-Arej.local>
2022-08-25 15:45:27 +0200

    app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AppSolbeg.Models;
+
+namespace AppSolbeg.Controllers;
+
+public class HomeController : Controller
+{
+    public string ReverseString(string t)
+    {
+        if (t == null || t.Length <= 1) 
+            return t;
+
+        return ReverseString(t.Substring(1)) + t[0];
+    }
+
+    [HttpGet]
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(string Text)
+    {
+        String reversedText = ReverseString(Text);
+
+        return Content(reversedText);
+    }
+
+}
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AppSolbeg.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
